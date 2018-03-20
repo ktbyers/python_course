ad6b79b94d82be21d20875eb1b05d1ee9991d989
Kirk Byers <ktbyers@twb-tech.com>
2018-03-19 20:46:01 -0700

    Updating class2 exercises

--- class2/ex4_snmp.py
+++ class2/ex4_snmp.py
@@ -1,35 +1,42 @@
 #!/usr/bin/env python
-'''
+"""
 Create a script that connects to both routers (pynet-rtr1 and pynet-rtr2) and
 prints out both the MIB2 sysName and sysDescr.
-'''
+"""
+from __future__ import print_function, unicode_literals
 import getpass
 import snmp_helper
 
 SYS_DESCR = '1.3.6.1.2.1.1.1.0'
 SYS_NAME = '1.3.6.1.2.1.1.5.0'
 
+
 def main():
-    '''
+    """
     Create a script that connects to both routers (pynet-rtr1 and pynet-rtr2) and
     prints out both the MIB2 sysName and sysDescr.
-    '''
-    ip_addr1 = raw_input("pynet-rtr1 IP address: ")
-    ip_addr2 = raw_input("pynet-rtr2 IP address: ")
+    """
+    try:
+        ip_addr1 = raw_input("pynet-rtr1 IP address: ")
+        ip_addr2 = raw_input("pynet-rtr2 IP address: ")
+    except NameError:
+        ip_addr1 = input("pynet-rtr1 IP address: ")
+        ip_addr2 = input("pynet-rtr2 IP address: ")
     community_string = getpass.getpass(prompt="Community string: ")
 
     pynet_rtr1 = (ip_addr1, community_string, 161)
     pynet_rtr2 = (ip_addr2, community_string, 161)
 
     for a_device in (pynet_rtr1, pynet_rtr2):
-        print "\n*********************"
+        print("\n*********************")
         for the_oid in (SYS_NAME, SYS_DESCR):
             snmp_data = snmp_helper.snmp_get_oid(a_device, oid=the_oid)
             output = snmp_helper.snmp_extract(snmp_data)
 
-            print output
-        print "*********************"
-    print
+            print(output)
+        print("*********************")
+    print()
+
 
 if __name__ == "__main__":
     main()
