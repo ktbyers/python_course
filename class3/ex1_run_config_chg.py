8d93f69760decc5596c4a4222aa39bfe7f95b4fc
Kirk Byers <ktbyers@twb-tech.com>
2018-03-21 16:35:45 -0700

    Class3 exercises

---
+++ class3/ex1_run_config_chg.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python
+"""
+Using SNMPv3 create a script that detects router configuration changes.
+
+If the running configuration has changed, then send an email notification to
+yourself identifying the router that changed and the time that it changed.
+
+In this exercise, you will possibly need to save data to an external file. One
+way you can accomplish this is by using a pickle file.
+"""
+from __future__ import print_function, unicode_literals
+
+try:
+    # PY2
+    import cPickle as pickle
+except ModuleNotFoundError:
+    # PY3
+    import pickle
+import os.path
+from getpass import getpass
+from datetime import datetime
+
+from snmp_helper import snmp_get_oid_v3, snmp_extract
+from email_helper import send_mail
+
+
+# Constants
+DEBUG = True
+
+# 300 seconds (converted to hundredths of seconds)
+RELOAD_WINDOW = 300 * 100
+
+# Uptime when running config last changed
+RUN_LAST_CHANGED = '1.3.6.1.4.1.9.9.43.1.1.1.0'
+
+# Relevant SNMP OIDs
+SYS_NAME = '1.3.6.1.2.1.1.5.0'
+SYS_UPTIME = '1.3.6.1.2.1.1.3.0'
+
+
+def obtain_saved_objects(file_name):
+    """
+    Read in previously saved objects from a pickle file
+
+    Returns a dict:
+    {
+      'device_name': device_object,
+      'device_name': device_object,
+    }
+
+    """
+
+    # Check that the pickle file exists
+    if not os.path.isfile(file_name):
+        return {}
+
+    # Read in any saved network devices
+    net_devices = {}
+    with open(file_name, 'rb') as f:
+        while True:
+            try:
+                tmp_device = pickle.load(f)
+                net_devices[tmp_device.device_name] = tmp_device
+            except EOFError:
+                break
+    return net_devices
+
+
+def send_notification(net_device):
+    """Send email notification regarding modified device."""
+    current_time = datetime.now()
+    sender = 'twb@twb-tech.com'
+    recipient = 'ktbyers@twb-tech.com'
+    subject = 'Device {} was modified'.format(net_device.device_name)
+
+    message = '''
+The running configuration of {} was modified.
+
+This change was detected at: {}
+
+'''.format(net_device.device_name, current_time)
+
+    if send_mail(recipient, subject, message, sender):
+        print('Email notification sent to {}'.format(recipient))
+        return True
+
+
+class NetworkDevice(object):
+    """
+    Simple object to store network device information
+
+    For an alternate solution, you could replace the class/objects with
+    a data structure that uses dictionaries.
+    """
+    def __init__(self, device_name, uptime, last_changed, config_changed=False):
+        self.device_name = device_name
+        self.uptime = uptime
+
+        # The uptime value in hundredths of seconds when running configuration
+        # was last changed
+        self.last_changed = last_changed
+        self.run_config_changed = config_changed
+
+
+def main():
+    """
+    Check if the running-configuration has changed, send an email notification when
+    this occurs.
+
+    Logic for detecting the running-config has changed:
+
+        Normal (non-reboot):
+
+            # Did RUN_LAST_CHANGED increase
+            if RUN_LAST_CHANGED > network_device_object.last_changed:
+                config_changed = True
+
+        Reboot case:
+
+            RUN_LAST_CHANGED decreases (i.e. < network_device_object.last_changed)
+
+            Right after reboot, RUN_LAST_CHANGED is updated upon
+            load of running-config from startup-config.
+
+            Create a grace window (RELOAD_WINDOW) for values of RUN_LAST_CHANGED.
+            In other words as long as RUN_LAST_CHANGED is <= RELOAD_WINDOW assume
+            no running-config changes.
+
+            If RUN_LAST_CHANGED is > RELOAD_WINDOW assume running-config was changed
+    """
+
+    # Pickle file for storing previous RunningLastChanged timestamp
+    net_dev_file = 'netdev.pkl'
+
+    # SNMPv3 Connection Parameters
+    try:
+        rtr1_ip_addr = raw_input("Enter pynet-rtr1 IP: ")
+        rtr2_ip_addr = raw_input("Enter pynet-rtr2 IP: ")
+    except NameError:
+        rtr1_ip_addr = input("Enter pynet-rtr1 IP: ")
+        rtr2_ip_addr = input("Enter pynet-rtr2 IP: ")
+    my_key = getpass(prompt="Auth + Encryption Key: ")
+
+    a_user = 'pysnmp'
+    auth_key = my_key
+    encrypt_key = my_key
+
+    snmp_user = (a_user, auth_key, encrypt_key)
+    pynet_rtr1 = (rtr1_ip_addr, 161)
+    pynet_rtr2 = (rtr2_ip_addr, 161)
+
+    print('\n*** Checking for device changes ***')
+    saved_devices = obtain_saved_objects(net_dev_file)
+    print("{} devices were previously saved\n".format(len(saved_devices)))
+
+    # Temporarily store the current devices in a dictionary
+    current_devices = {}
+
+    # Connect to each device / retrieve last_changed time
+    for a_device in (pynet_rtr1, pynet_rtr2):
+        snmp_results = []
+        for oid in (SYS_NAME, SYS_UPTIME, RUN_LAST_CHANGED):
+            try:
+                value = snmp_extract(snmp_get_oid_v3(a_device, snmp_user, oid=oid))
+                snmp_results.append(int(value))
+            except ValueError:
+                snmp_results.append(value)
+        device_name, uptime, last_changed = snmp_results
+        if DEBUG:
+            print("\nConnected to device = {0}".format(device_name))
+            print("Last changed timestamp = {0}".format(last_changed))
+            print("Uptime = {0}".format(uptime))
+
+        # see if this device has been previously saved
+        if device_name in saved_devices:
+            saved_device = saved_devices[device_name]
+            dots_to_print = (35 - len(device_name)) * '.'
+            print("{} {}".format(device_name, dots_to_print), end=' ')
+
+            # Check for a reboot (did uptime decrease or last_changed decrease?)
+            if uptime < saved_device.uptime or last_changed < saved_device.last_changed:
+                if last_changed <= RELOAD_WINDOW:
+                    print("DEVICE RELOADED...not changed")
+                    current_devices[device_name] = NetworkDevice(device_name, uptime,
+                                                                 last_changed, False)
+                else:
+                    print("DEVICE RELOADED...and changed")
+                    current_devices[device_name] = NetworkDevice(device_name, uptime,
+                                                                 last_changed, True)
+                    send_notification(current_devices[device_name])
+            elif last_changed == saved_device.last_changed:
+                # running-config last_changed is the same
+                print("not changed")
+                current_devices[device_name] = NetworkDevice(device_name, uptime,
+                                                             last_changed, False)
+            elif last_changed > saved_device.last_changed:
+                # running-config was modified
+                print("CHANGED")
+                current_devices[device_name] = NetworkDevice(device_name, uptime,
+                                                             last_changed, True)
+                send_notification(current_devices[device_name])
+            else:
+                raise ValueError()
+        else:
+            # New device, just save it
+            dots_to_print = (35 - len(device_name)) * '.'
+            print("{} {}".format(device_name, dots_to_print), end=' ')
+            print("saving new device")
+            current_devices[device_name] = NetworkDevice(device_name, uptime, last_changed, False)
+
+    # Write the devices to pickle file
+    with open(net_dev_file, 'wb') as f:
+        for dev_obj in current_devices.values():
+            pickle.dump(dev_obj, f)
+    print()
+
+
+if __name__ == '__main__':
+    main()
---
+++ class3/ex2_snmp_int_graph.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+'''
+Using SNMPv3 create two SVG image files.
+
+The first image file should graph the input and output octets on interface FA4
+on pynet-rtr1 every five minutes for an hour. Use the pygal library to create
+the SVG graph file. Note, you should be doing a subtraction here (i.e. the
+input/output octets transmitted during this five minute interval).
+
+The second SVG graph file should be the same as the first except graph the
+unicast packets received and transmitted.
+
+The relevant OIDs are as follows:
+
+('ifDescr_fa4', '1.3.6.1.2.1.2.2.1.2.5')
+('ifInOctets_fa4', '1.3.6.1.2.1.2.2.1.10.5')
+('ifInUcastPkts_fa4', '1.3.6.1.2.1.2.2.1.11.5')
+('ifOutOctets_fa4', '1.3.6.1.2.1.2.2.1.16.5'),
+('ifOutUcastPkts_fa4', '1.3.6.1.2.1.2.2.1.17.5')
+'''
+
+from snmp_helper import snmp_get_oid_v3, snmp_extract
+import line_graph
+import time
+from getpass import getpass
+
+
+def get_interface_stats(snmp_device, snmp_user, stat_type, row_number):
+    '''
+    stat_type can be 'in_octets, out_octets, in_ucast_pkts, out_ucast_pkts
+
+    returns the counter value as an integer
+    '''
+
+    oid_dict = {
+        'in_octets':    '1.3.6.1.2.1.2.2.1.10',
+        'out_octets':   '1.3.6.1.2.1.2.2.1.16',
+        'in_ucast_pkts':    '1.3.6.1.2.1.2.2.1.11',
+        'out_ucast_pkts':    '1.3.6.1.2.1.2.2.1.17',
+    }
+
+    if not stat_type in oid_dict.keys():
+        raise ValueError("Invalid value for stat_type: {}" % stat_type)
+
+    # Make sure row_number can be converted to an int
+    row_number = int(row_number)
+
+    # Append row number to OID
+    oid = oid_dict[stat_type]
+    oid = oid + '.' + str(row_number)
+
+    snmp_data = snmp_get_oid_v3(snmp_device, snmp_user, oid)
+    return int(snmp_extract(snmp_data))
+
+
+def main():
+    '''
+    Connect to router via SNMPv3.
+
+    Create two graphs in/out octets and in/out packets
+    '''
+    debug = False
+
+    # SNMPv3 Connection Parameters
+    rtr1_ip_addr = raw_input("Enter pynet-rtr1 IP: ")
+    my_key = getpass(prompt="Auth + Encryption Key: ")
+
+    a_user = 'pysnmp'
+    auth_key = my_key
+    encrypt_key = my_key
+
+    snmp_user = (a_user, auth_key, encrypt_key)
+    snmp_device = (rtr1_ip_addr, 161)
+
+    # Fa4 is in row number5 in the MIB-2 interfaces table
+    row_number = 5
+    graph_stats = {
+        "in_octets": [],
+        "out_octets": [],
+        "in_ucast_pkts": [],
+        "out_ucast_pkts": [],
+    }
+    base_count_dict = {}
+
+    # Enter a loop gathering SNMP data every 5 minutes for an hour.
+    for time_track in range(0, 65, 5):
+        print "\n%20s %-60s" % ("time", time_track)
+
+        # Gather SNMP statistics for these four fields
+        for entry in ("in_octets", "out_octets", "in_ucast_pkts", "out_ucast_pkts"):
+            # Retrieve the SNMP data
+            snmp_retrieved_count = get_interface_stats(snmp_device, snmp_user, entry, row_number)
+            # Get the base counter value
+            base_count = base_count_dict.get(entry)
+            if base_count:
+                # Save the data to graph_stats dictionary
+                graph_stats[entry].append(snmp_retrieved_count - base_count)
+                print "%20s %-60s" % (entry, graph_stats[entry][-1])
+            # Update the base counter value
+            base_count_dict[entry] = snmp_retrieved_count
+        time.sleep(10)
+
+    print
+    if debug:
+        print graph_stats
+    x_labels = []
+    for x_label in range(5, 65, 5):
+        x_labels.append(str(x_label))
+    if debug:
+        print x_labels
+
+    # Create the graphs
+    if line_graph.twoline("pynet-rtr1-octets.svg", "pynet-rtr1 Fa4 Input/Output Bytes",
+                          graph_stats["in_octets"], "In Octets", graph_stats["out_octets"],
+                          "Out Octets", x_labels):
+        print "In/Out Octets graph created"
+
+    if line_graph.twoline("pynet-rtr1-pkts.svg", "pynet-rtr1 Fa4 Input/Output Unicast Packets",
+                          graph_stats["in_ucast_pkts"], "In Packets", graph_stats["out_ucast_pkts"],
+                          "Out Packets", x_labels):
+        print "In/Out Packets graph created"
+    print
+
+
+if __name__ == '__main__':
+    main()
